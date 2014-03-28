e5f74ae915e376ac3d44d3a3e35c64022e2d4630
ManinderPal Singh <manni.cse@gmail.com>
2014-03-28 11:07:50 +0200

    Added Bar Code Scanner for scanning bar code.

--- src/com/example/armuseum/ARMuseum.java
+++ src/com/example/armuseum/ARMuseum.java
@@ -1,15 +1,33 @@
 package com.example.armuseum;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ARMuseum extends Activity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
+public class ARMuseum extends Activity implements OnClickListener {
+
+	private Button scanBtn;
+	private TextView formatTxt, contentTxt;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_armuseum);
+		
+		scanBtn = (Button)findViewById(R.id.scan_button);
+		formatTxt = (TextView)findViewById(R.id.scan_format);
+		contentTxt = (TextView)findViewById(R.id.scan_content);
+		
+		scanBtn.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +35,34 @@ public class ARMuseum extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.armuseum, menu);
 		return true;
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		// TODO Auto-generated method stub
+		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		if (scanningResult != null) {
+			//we have a result
+			String scanContent = scanningResult.getContents();
+			String scanFormat = scanningResult.getFormatName();
+			
+			formatTxt.setText("FORMAT: " + scanFormat);
+			contentTxt.setText("CONTENT: " + scanContent);
+			
+			}else{
+			    Toast toast = Toast.makeText(getApplicationContext(), 
+			            "No scan data received!", Toast.LENGTH_SHORT);
+			        toast.show();
+			    }
+	}
+
+	@Override
+	public void onClick(View v) {
+		if(v.getId()==R.id.scan_button){
+			//scan
+			IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+			scanIntegrator.initiateScan();
+			}
+		
 	}
 
 }
