f0d26aaa8a8d555f56366b700e412b8ec7c9a665
ManinderPal Singh <manni.cse@gmail.com>
2014-03-28 16:29:31 +0200

    Added QR Code scanner to the App.

--- src/com/example/armuseum/ARMuseum.java
+++ src/com/example/armuseum/ARMuseum.java
@@ -3,10 +3,13 @@ package com.example.armuseum;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,54 +18,62 @@ import com.google.zxing.integration.android.IntentResult;
 
 public class ARMuseum extends Activity implements OnClickListener {
 
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+
+	private CharSequence mDrawerTitle;
+	private CharSequence mTitle;
+	private String[] mPlanetTitles;
+
 	private Button scanBtn;
 	private TextView formatTxt, contentTxt;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_armuseum);
-		
-		scanBtn = (Button)findViewById(R.id.scan_button);
-		formatTxt = (TextView)findViewById(R.id.scan_format);
-		contentTxt = (TextView)findViewById(R.id.scan_content);
-		
-		scanBtn.setOnClickListener(this);
+
+		scanBtn = (Button) findViewById(R.id.scan_button);
+		formatTxt = (TextView) findViewById(R.id.scan_format);
+		contentTxt = (TextView) findViewById(R.id.scan_content);
+
+		// scanBtn.setOnClickListener(this);
+		IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+		scanIntegrator.initiateScan();
 	}
 
+	 
+
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.armuseum, menu);
-		return true;
-	}
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent intent) {
 		// TODO Auto-generated method stub
-		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		IntentResult scanningResult = IntentIntegrator.parseActivityResult(
+				requestCode, resultCode, intent);
 		if (scanningResult != null) {
-			//we have a result
+			// we have a result
 			String scanContent = scanningResult.getContents();
 			String scanFormat = scanningResult.getFormatName();
-			
+
 			formatTxt.setText("FORMAT: " + scanFormat);
 			contentTxt.setText("CONTENT: " + scanContent);
-			
-			}else{
-			    Toast toast = Toast.makeText(getApplicationContext(), 
-			            "No scan data received!", Toast.LENGTH_SHORT);
-			        toast.show();
-			    }
+
+		} else {
+			Toast toast = Toast.makeText(getApplicationContext(),
+					"No scan data received!", Toast.LENGTH_SHORT);
+			toast.show();
+		}
 	}
 
 	@Override
 	public void onClick(View v) {
-		if(v.getId()==R.id.scan_button){
-			//scan
+		if (v.getId() == R.id.scan_button) {
+			// scan
 			IntentIntegrator scanIntegrator = new IntentIntegrator(this);
 			scanIntegrator.initiateScan();
-			}
-		
+		}
+
 	}
 
 }
