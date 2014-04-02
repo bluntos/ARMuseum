56d99df83a1155e6552765d83e3eb674e9813ffe
ManinderPal Singh <manni.cse@gmail.com>
2014-04-02 23:23:49 +0300

    Added Splash screen to the App.

--- src/com/example/armuseum/ARMuseum.java
+++ src/com/example/armuseum/ARMuseum.java
@@ -1,14 +1,18 @@
 package com.example.armuseum;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,6 +32,10 @@ public class ARMuseum extends Activity implements OnClickListener {
 
 	private Button scanBtn;
 	private TextView formatTxt, contentTxt;
+	
+	private ImageView mSplashScreen = null;
+	
+	private ScheduledExecutorService scheduleTaskExecutor;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +45,43 @@ public class ARMuseum extends Activity implements OnClickListener {
 		scanBtn = (Button) findViewById(R.id.scan_button);
 		formatTxt = (TextView) findViewById(R.id.scan_format);
 		contentTxt = (TextView) findViewById(R.id.scan_content);
+		
+		mSplashScreen = (ImageView)findViewById(R.id.imageSplash);
+		
+		scheduleTaskExecutor = Executors.newSingleThreadScheduledExecutor();
+		scheduleTaskExecutor.schedule(new DismissSplash(this), 5, TimeUnit.SECONDS);
+	}
 
+	class DismissSplash implements Runnable
+	{
+		private final ARMuseum mMainActivity;
+
+		public DismissSplash(ARMuseum mainActivity)
+		{
+			mMainActivity = mainActivity;
+		}
+
+		public void run()
+		{
+			mMainActivity.runOnUiThread(new DoDismissSplash());
+		}
+		
+		class DoDismissSplash implements Runnable
+		{
+			public void run()
+			{
+				mMainActivity.onSplashDone();
+			}
+		}
+	}
+	
+	public void onSplashDone()
+	{
+		mSplashScreen.setVisibility(View.GONE);
+		
 		// scanBtn.setOnClickListener(this);
-		IntentIntegrator scanIntegrator = new IntentIntegrator(this);
-		scanIntegrator.initiateScan();
+				IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+				scanIntegrator.initiateScan();
 	}
 
 	 
